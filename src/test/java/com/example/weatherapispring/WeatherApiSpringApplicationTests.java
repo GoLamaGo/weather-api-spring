119a39edd174247577acafd4ba4b99d29979815d
Andrew <aka.slurm@gmail.com>
2022-11-18 18:33:23 +0300

    Initial commit

---
+++ src/test/java/com/example/weatherapispring/WeatherApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
